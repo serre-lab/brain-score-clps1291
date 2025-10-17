d38e26593357eaa06dd2d9a95215a25374640a3d
Alekh Karkada Ashok <alekhka@gmail.com>
2025-10-17 18:05:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.md') as readme_file:
 
 requirements = [
     "numpy>=1.17",
-    "brainio @ git+https://github.com/brain-score/brainio",
+    "brainscore-brainio @ git+https://github.com/brain-score/brainio",
     "importlib-metadata<5",  # workaround to https://github.com/brain-score/brainio/issues/28
     "scikit-learn",  # 0.24 breaks pls regression
     "scipy",
